aac2f15c1f877066cfa21c43bfb480751563e98e
yamfgr2018 <44949339+yamgit2018@users.noreply.github.com>
2025-12-02 13:12:23 -0500

    Agregamos operadores de comparacion con ipaddress

--- Roadmap/01 - OPERADORES Y ESTRUCTURAS DE CONTROL/python/yamgit2018.py
+++ Roadmap/01 - OPERADORES Y ESTRUCTURAS DE CONTROL/python/yamgit2018.py
@@ -1,14 +1,27 @@
 import ipaddress
 """Operadores"""
 """Operadores Aritmeticos"""
-ipv4=ipaddress.IPv4Address("172.16.0.10")
-ipv4_entero=int(ipv4)
-print(f"Como Objecto ipv4: {ipv4} \nComo Entero  ipv4: {ipv4_entero}")
+# ipv4=ipaddress.IPv4Address("172.16.0.10")
+# ipv4_entero=int(ipv4)
+# print(f"Como Objecto ipv4: {ipv4} \nComo Entero  ipv4: {ipv4_entero}")
 
 """print(f"Le sumamos 100 a la ip 172.16.0.10: {ipv4+100} ")
 print(f"Le restamos 8 a la ip 172.16.0.10: {ipv4-8} ")"""
 
 
+"""Operadores de Comparacion"""
+# ip1=ipaddress.IPv4Address("10.0.0.100")
+# ip2=ipaddress.IPv4Address("10.0.0.100")
+
+# if ip1>ip2:
+#     print(f"La ip {ip1} es mayor que la ip {ip2}")
+# elif ip1<ip2:
+#     print(f"La ip {ip1} es menor que la ip {ip2}")
+# else:
+#     print(f"La ip {ip1} es igual que la ip {ip2}")
+
+
+"""Operadores Logicos"""
 
 
 
